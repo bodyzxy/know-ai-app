476b43e0d1525bc05d5ff9a43df17ec3acb9995d
NingnNing0111 <zdncode@gmail.com>
2024-05-29 23:56:12 +0800

    add some dependencies

--- lib/constant/globals.dart
+++ lib/constant/globals.dart
@@ -5,13 +5,6 @@ class Globals {
 
   static final List<MenuOptionsModel> languageOptions = [
     MenuOptionsModel(key: "zh", value: "中文"), //Chinese
-    MenuOptionsModel(key: "de", value: "Deutsche"), //German
     MenuOptionsModel(key: "en", value: "English"), //English
-    MenuOptionsModel(key: "es", value: "Español"), //Spanish
-    MenuOptionsModel(key: "fr", value: "Français"), //French
-    MenuOptionsModel(key: "hi", value: "हिन्दी"), //Hindi
-    MenuOptionsModel(key: "ja", value: "日本語"), //Japanese
-    MenuOptionsModel(key: "pt", value: "Português"), //Portuguese
-    MenuOptionsModel(key: "ru", value: "русский"), //Russian
   ];
 }
